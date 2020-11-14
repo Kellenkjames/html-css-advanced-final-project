aa93c0bfe09eb7c813397a7c1d00cf4cb0c4d152
Kellen <kellenkjames@gmail.com>
2020-11-14 12:07:21 -0800

    added lax.js (for scroll animations)

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@ let cartNotification;
 
 function buyNow(e){
   cartNotification.innerText = Number(cartNotification.innerText) + 1;
-  const removeItemButton = document.querySelector(`.products__list__link__remove[data-id='${e.target.dataset.id}]'`)
-  console.log(removeItemButton);
+  const buyNowButton = e.target;
+  const removeItemButton = document.querySelector(`.products__list__link__remove[data-id="${e.target.dataset.id}"]`)
+  buyNowButton.classList.add("hidden");
+  removeItemButton.classList.remove("hidden");
 }
 
 function removeItem(e){
@@ -21,3 +23,26 @@ window.onload = () => {
     removeItemButtons[idx].addEventListener('click', removeItem)
   }
 }
+
+/* LAX JS */
+< !--JS -->
+    <script>
+        window.onload = function() {
+            lax.init()
+
+    // Add a driver that we use to control our animations
+    lax.addDriver('scrollY', function() {
+      return document.body.scrollTop
+    })
+
+    // Add animation bindings to elements
+    lax.addElements('.selector', {
+            scrollY: {
+            translateX: [
+          ["elInY", "elCenterY", "elOutY"],
+          [0, 'screenWidth/2', 'screenWidth'],
+        ]
+      }
+    }
+  })
+</script>
